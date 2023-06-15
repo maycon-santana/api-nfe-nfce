abfc22a53493b36c09d9cee88ebc46cf8749c0a4
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-06-15 20:40:54 -0300

    Instalando a dependencia para nfe

---
+++ src/main/java/br/com/mayconinforgames/exemploapinfe/Teste.java
@@ -0,0 +1,17 @@
+package br.com.mayconinforgames.exemploapinfe;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        try {
+            emiteNfe();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void emiteNfe() throws Exception {
+        
+    }
+
+}
