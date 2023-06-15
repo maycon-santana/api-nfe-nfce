88393b70316846308369282abcf4370088c9c4c2
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-06-15 20:19:15 -0300

    Criando projeto

---
+++ src/test/java/br/com/mayconinforgames/exemploapinfe/ExemploApiNfeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayconinforgames.exemploapinfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploApiNfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
